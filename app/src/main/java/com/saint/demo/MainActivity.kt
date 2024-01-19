32dfed0ed78a1f4486d6bcf0017040d548b3940a
Saint <saint.li@qq.com>
2024-01-19 12:02:35 +0800

    gradle升级

---
+++ app/src/main/java/com/saint/demo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.saint.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saint.push_lib.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
